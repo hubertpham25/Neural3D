bd4cfd812f7c5446ac3eed729bd30057ad15e6df
peanutgallery <phamhubert820@gmail.com>
2025-07-17 22:10:16 -0400

    start

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/hello", methods=["GET"])
+def hello():
+    return jsonify({"msg": "Hello World"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
